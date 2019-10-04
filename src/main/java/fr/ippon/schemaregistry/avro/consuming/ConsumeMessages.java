a1fefbd228b4bec5c250ff9360035daeb8fb21e0
Mohamed HILIA <mhilia@MacBook-Pro-de-Mohamed.local>
2019-10-04 15:37:11 +0200

    Initial commit

---
+++ src/main/java/fr/ippon/schemaregistry/avro/consuming/ConsumeMessages.java
@@ -0,0 +1,59 @@
+package fr.ippon.schemaregistry.avro.consuming;
+
+
+import fr.ippon.schemaregistry.avro.Consumer;
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumeMessages {
+
+
+    private static final String TOPIC = "topic.consumers.eu";
+    private static final String SCHEMA_REGISTRY_URL = "http://localhost:8081";
+    private static final String BOOTSTRAP_SERVERS = "localhost:9092";
+
+    Logger log = LoggerFactory.getLogger(ConsumeMessages.class);
+
+    public static void main(String[] args) {
+
+
+        Properties props = new Properties();
+
+        props.setProperty("group.id", "eu.group.1");
+        props.setProperty("enable.auto.commit", "true");
+        props.setProperty("auto.offset.reset", "earliest");
+        props.put("bootstrap.servers", BOOTSTRAP_SERVERS);
+
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaAvroDeserializer");
+        props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, SCHEMA_REGISTRY_URL);
+
+
+        KafkaConsumer<String, Consumer> kafkaConsumer = new KafkaConsumer<String, Consumer>(props);
+        kafkaConsumer.subscribe(Arrays.asList(TOPIC));
+
+
+        while (true) {
+            ConsumerRecords<String, Consumer> records = kafkaConsumer.poll(Duration.ofMillis(1000));
+            log.info("Number of Records : " + records.count());
+
+            if (records.count() > 0)
+                for (ConsumerRecord<String, Consumer> record : records) {
+
+                    log.info("Offset = %d, Key = %s, Value = %s %n", record.offset(), record.key(), record.value());
+
+                }
+        }
+
+
+    }
+}
---
+++ src/main/java/fr/ippon/schemaregistry/avro/production/ProduceConsumers.java
@@ -0,0 +1,102 @@
+package fr.ippon.schemaregistry.avro.production;
+
+
+ import fr.ippon.schemaregistry.avro.Civility;
+ import fr.ippon.schemaregistry.avro.Consumer;
+ import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+ import org.apache.kafka.clients.producer.KafkaProducer;
+ import org.apache.kafka.clients.producer.ProducerConfig;
+ import org.apache.kafka.clients.producer.ProducerRecord;
+ import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+
+public class ProduceConsumers {
+
+
+    // CONFIG PART
+    private static final String TOPIC = "topic.consumers.eu";
+    private static final String BOOTSTRAP_SERVERS = "localhost:9092";
+    private static final String SCHEMA_REGISTRY_URL = "http://localhost:8081";
+    private static final Class<StringSerializer> KEY_STRING_SERIALIZER_CLASS = StringSerializer.class;
+    private static final Class<KafkaAvroSerializer> VALUE_AVRO_SERIALIZER = KafkaAvroSerializer.class;
+    private static final int NUM_RECORD = 10000;
+
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+
+        //String EH_SASL = "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"login\" password=\"pwd\";";
+
+        Properties props = new Properties();
+        props.put("bootstrap.servers", BOOTSTRAP_SERVERS);
+        //props.put("sasl.mechanism", "PLAIN");
+        //props.put("security.protocol", "SASL_PLAINTEXT");
+        //props.put("sasl.jaas.config", EH_SASL);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KEY_STRING_SERIALIZER_CLASS);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, VALUE_AVRO_SERIALIZER);
+        props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, SCHEMA_REGISTRY_URL);
+        props.put("acks" , "all");
+
+
+        String consumerAvroSchema = "{\n" +
+                " \"type\": \"record\",\n" +
+                " \"name\": \"Consumer\",\n" +
+                " \"namespace\": \"org.ippon.schemaregistry.avro\",\n" +
+                " \"fields\": [\n" +
+                "   { \"name\": \"consumer_id\" , \"type\": \"string\"},\n" +
+                "   { \"name\": \"last_name\"   ,\"type\": \"int\"},\n" +
+                "   { \"name\": \"first_name\", \"type\": \"string\" },\n" +
+                "   { \"name\": \"city\", \"type\": \"string\"},\n" +
+                "   { \n" +
+                "\"name\": \"civility\", \n" +
+                "\"type\": {\"type\": \"enum\",\"name\": \"Civility\",\"symbols\": [\"MISS\", \"MR\", \"MS\"]}\n" +
+                "   },\n" +
+                "   {\"name\": \"country\",\"type\": \"int\" },\n" +
+                "   {\"name\": \"phoneNumber\", \"type\": [ \"string\",\"null\" ]}\n" +
+                " ]\n" +
+                "}\n";
+
+
+        Future<RecordMetadata> future = null;
+
+        KafkaProducer<String, Consumer> producer = new KafkaProducer<String, Consumer>(props);
+
+
+        long start = System.currentTimeMillis();
+
+        Random r  = new Random();
+
+        for(int i=0; i< NUM_RECORD ; i++){
+
+            String consumerId = "eu#" + r.nextInt(5) + ( System.currentTimeMillis() - Long.MAX_VALUE);
+            System.out.println(consumerId);
+
+            final Consumer consumer = Consumer.newBuilder()
+                    .setConsumerId(consumerId)
+                    .setCivility(Civility.MISS)
+                    .setLastName("Z")
+                    .setFirstName("X")
+                    .setCountry("FR")
+                    .setCity("Paris")
+                    .setEmail("*****@ippon.tech")
+                    .setPhoneNumber("+336xxxxxxxx")
+                    .build();
+
+            final ProducerRecord<String, Consumer> record = new ProducerRecord<String, Consumer>(TOPIC, consumer.getConsumerId().toString(), consumer);
+            future = producer.send(record);
+            System.out.printf("Message : TOPIC : %s, OFFSET : %s, PARTITION : %s%n", future.get().topic(),future.get().offset(),future.get().partition());
+            producer.flush();
+        }
+        long end = System.currentTimeMillis();
+
+        System.out.println("End in : " + ((end - start)/1000) + " seconds") ;
+
+    }
+}
